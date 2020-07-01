cac3d817d1d879777796816f84185951cae485f1
nbur4556 <nbur4556@gmail.com>
2020-06-30 20:54:12 -0600

    Added listeners to play, pause and stop control buttons

--- scripts/index-script.js
+++ scripts/index-script.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
+    var tempo = 120;
+    var noteblocks = $(".note-block");
 
+    //Listeners
+    $("#play-btn").on("click", function () {
+        playSequence();
+    });
+    $("#stop-btn").on("click", function () {
+        stopSequence();
+    });
+    $("#pause-btn").on("click", function () {
+        stopSequence();
+    })
 });
+
+function playSequence() {
+    alert("play");
+}
+
+function stopSequence() {
+    alert("stop");
+}
